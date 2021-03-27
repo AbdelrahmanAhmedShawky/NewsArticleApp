1485b8e7a4f98943a265087d85c894579378492d
Abdelrahman <abdelrahmanmacios@gmail.com>
2021-03-27 22:01:24 +0200

    refactor code

--- NewsArticleApp/NewsArticleApp/NewsList/View/NewsListView.swift
+++ NewsArticleApp/NewsArticleApp/NewsList/View/NewsListView.swift
@@ -4,71 +4,105 @@ struct NewsListView: View {
     @ObservedObject var viewModel = NewsResultViewModel()
     @State private var showCancelButton: Bool = false
     
+    @ViewBuilder
+    var searchView:some View {
+        
+        HStack {
+            HStack {
+                Image(systemName: "magnifyingglass")
+                TextField("search", text: self.$viewModel.searchTerm, onEditingChanged: { isEditing in
+                    self.showCancelButton = true
+                }, onCommit: {
+                    self.viewModel.searchingNewsList()
+                    self.showCancelButton = true
+                }).foregroundColor(.primary)
+                
+                Button(action: {
+                    UIApplication.shared.endEditing(true)
+                    self.viewModel.searchTerm = ""
+                    self.viewModel.getNewsList()
+                    self.viewModel.searchNewsList.removeAll()
+                    self.showCancelButton = false
+                }) {
+                    Image(systemName: "xmark.circle.fill").opacity(self.viewModel.searchTerm == "" ? 0 : 1)
+                }
+            }
+            .padding(EdgeInsets(top: 8, leading: 6, bottom: 8, trailing: 6))
+            .foregroundColor(.secondary)
+            .background(Color(.secondarySystemBackground))
+            .cornerRadius(10.0)
+            
+            if self.showCancelButton  {
+                Button("Cancel") {
+                    UIApplication.shared.endEditing(true)
+                    self.viewModel.searchTerm = ""
+                    self.viewModel.getNewsList()
+                    self.viewModel.searchNewsList.removeAll()
+                    self.showCancelButton = false
+                }
+                .foregroundColor(Color(.systemBlue))
+            }
+        }
+        .padding(.horizontal)
+        .navigationBarHidden(self.showCancelButton)
+    }
+    
+    @ViewBuilder
+    var segmantControl:some View {
+        Picker(selection: $viewModel.selection, label: Text("")) {
+            ForEach(0..<viewModel.selectedCategories.count, id: \.self) { index in
+                Text(self.viewModel.selectedCategories[index]).tag(index)
+            }
+        }.onChangeBackwardsCompatible(of: viewModel.selection) { (newIndex) in
+            self.viewModel.searchTerm.isEmpty ?
+                viewModel.getNewsList() : viewModel.searchingNewsList()
+        }
+        .pickerStyle(SegmentedPickerStyle()).padding()
+    }
+    
+    var emptyListView: some View {
+        Text("no data...").frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height/1.5, alignment: .center)
+    }
+    
+    var NewsListView: some View {
+        List(viewModel.searchTerm.isEmpty ? self.viewModel.newsList : self.viewModel.searchNewsList,id:\.description) { item in
+            NewsViewCell(item: item)
+        }
+    }
+    
+    @ViewBuilder
+    var listView: some View {
+        if viewModel.searchTerm.isEmpty {
+            if self.viewModel.newsList.isEmpty {
+                emptyListView
+            } else {
+                NewsListView
+            }
+        }else {
+            if self.viewModel.searchNewsList.isEmpty {
+                emptyListView
+            } else {
+                NewsListView
+            }
+        }
+        
+    }
+    
     var body: some View {
         LoadingView(isShowing: .constant($viewModel.isShowLoader.wrappedValue)) {
             NavigationView {
                 Group {
                     VStack {
-                        // Search view
-                        HStack {
-                            HStack {
-                                Image(systemName: "magnifyingglass")
-                                TextField("search", text: self.$viewModel.searchTerm, onEditingChanged: { isEditing in
-//                                    self.viewModel.searchingNewsList()
-                                    self.showCancelButton = true
-                                }, onCommit: {
-                                    self.viewModel.searchingNewsList()
-                                    self.showCancelButton = true
-                                }).foregroundColor(.primary)
-                                
-                                Button(action: {
-                                    UIApplication.shared.endEditing(true)
-                                    self.viewModel.searchTerm = ""
-                                    self.viewModel.getNewsList()
-                                    self.viewModel.searchNewsList.removeAll()
-                                    self.showCancelButton = false
-                                }) {
-                                    Image(systemName: "xmark.circle.fill").opacity(self.viewModel.searchTerm == "" ? 0 : 1)
-                                }
-                            }
-                            .padding(EdgeInsets(top: 8, leading: 6, bottom: 8, trailing: 6))
-                            .foregroundColor(.secondary)
-                            .background(Color(.secondarySystemBackground))
-                            .cornerRadius(10.0)
-                            
-                            if self.showCancelButton  {
-                                Button("Cancel") {
-                                    UIApplication.shared.endEditing(true)
-                                    self.viewModel.searchTerm = ""
-                                    self.viewModel.getNewsList()
-                                    self.viewModel.searchNewsList.removeAll()
-                                    self.showCancelButton = false
-                                }
-                                .foregroundColor(Color(.systemBlue))
-                            }
-                        }
-                        .padding(.horizontal)
-                        .navigationBarHidden(self.showCancelButton)
-                        Picker(selection: $viewModel.selection, label: Text("")) {
-                            ForEach(0..<viewModel.selectedCategories.count, id: \.self) { index in
-                                Text(self.viewModel.selectedCategories[index]).tag(index)
-                            }
-                        }.onChangeBackwardsCompatible(of: viewModel.selection) { (newIndex) in
-                            self.viewModel.searchNewsList.isEmpty ?
-                                viewModel.getNewsList() : viewModel.searchingNewsList()
-                        }
-                        .pickerStyle(SegmentedPickerStyle()).padding()
-                        List(self.viewModel.searchNewsList.isEmpty ? self.viewModel.newsList : self.viewModel.searchNewsList,id:\.description) { item in
-                            NewsViewCell(item: item)
-                        }
-                        .navigationBarTitle("News Articls")
-                        .resignKeyboardOnDragGesture()
+                        searchView
+                        segmantControl
+                        listView
                     }
-                    
-                }}
-                .onAppear {
-                    self.viewModel.getNewsList()
-                }
+                }.navigationBarTitle("News Articls")
+                .resignKeyboardOnDragGesture()
+            }
+            .onAppear {
+                self.viewModel.getNewsList()
+            }
         }.alert(isPresented: $viewModel.isShowAlert) {
             Alert(
                 title: Text(""),
